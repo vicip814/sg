cbbc768c3059862d51c94e4f9dccc20cb5d64966
vicip814 <65480946+vicip814@users.noreply.github.com>
2020-10-19 15:19:19 +0800

    Update interface.js

--- interface.js
+++ interface.js
@@ -333,7 +333,7 @@ SelectorGadget.prototype.showTest = function(e) {
   doc.open();
   var x  = "";
   jQuery(path).each(function( index ){
-	  x =  x + jQuery(this).text() + "\n";
+	  x =  x + jQuery(this).text().trim() + "\n";
   });  
   doc.write('<textarea style="width:100%; height : 100%">' + x + '</textarea>');
 	
